de8d8db790788deab1c3e6ebc58571ba6cef2d33
Tran Toan Van <vantt@anphabe.com>
2020-02-12 14:28:28 +0700

    deploy

---
+++ precache-manifest.d6db86c2c70f0d50d24837b6d689186c.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "57a89518184d78b0caef",
+    "url": "/vueScanAgent/assets/js/about.18b810c1.js"
+  },
+  {
+    "revision": "1b93463fb0359e50a2ba",
+    "url": "/vueScanAgent/assets/css/app.26f55e0a.css"
+  },
+  {
+    "revision": "1b93463fb0359e50a2ba",
+    "url": "/vueScanAgent/assets/js/app.eb739664.js"
+  },
+  {
+    "revision": "dcf6e626e8913d5a1370",
+    "url": "/vueScanAgent/assets/css/chunk-vendors.0792aaf8.css"
+  },
+  {
+    "revision": "dcf6e626e8913d5a1370",
+    "url": "/vueScanAgent/assets/js/chunk-vendors.14ed77ed.js"
+  },
+  {
+    "revision": "b76b0645d3ba50d44def821b619ab0b6",
+    "url": "/vueScanAgent/assets/media/beep.b76b0645.mp3"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/vueScanAgent/assets/fonts/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/vueScanAgent/assets/fonts/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/vueScanAgent/assets/fonts/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/vueScanAgent/assets/fonts/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/vueScanAgent/assets/fonts/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/vueScanAgent/assets/fonts/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/vueScanAgent/assets/fonts/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/vueScanAgent/assets/fonts/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/vueScanAgent/assets/fonts/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/vueScanAgent/assets/fonts/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/vueScanAgent/assets/fonts/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/vueScanAgent/assets/fonts/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/vueScanAgent/assets/img/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/vueScanAgent/assets/img/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/vueScanAgent/assets/img/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/vueScanAgent/assets/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/vueScanAgent/assets/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/vueScanAgent/assets/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/vueScanAgent/assets/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "f6674430d13fb17264e1df6d69ef27a1",
+    "url": "/vueScanAgent/index.html"
+  },
+  {
+    "revision": "b76b0645d3ba50d44def821b619ab0b6",
+    "url": "/vueScanAgent/beep.mp3"
+  },
+  {
+    "revision": "1310a0db08d897836dc2e16349df1a97",
+    "url": "/vueScanAgent/config/defaultScanActions.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vueScanAgent/robots.txt"
+  }
+];
