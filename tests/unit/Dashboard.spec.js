1c343c7d3baa260a3d748b612076a14a7b4cb55a
Tran Toan Van <trantoanvan@outlook.com>
2021-03-13 23:13:34 +0700

    Complete unit tests for Dashboard Component

--- tests/unit/Dashboard.spec.js
+++ tests/unit/Dashboard.spec.js
@@ -3,8 +3,8 @@ import Vuetify from "vuetify";
 import Vuex from "vuex";
 import { createStore } from "@/store";
 import { scanActions } from "../data/scanActions";
+import router from "@/router";
 import Dashboard from "@/components/Dashboard";
-import { nextTick } from 'vue'
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
@@ -15,10 +15,8 @@ const vuetify = new Vuetify();
 const createComponent = () => {
   return mount(Dashboard, {
     store,
+    router,
     vuetify,
-    stubs: {
-      RouterLink: RouterLinkStub
-    },
     localVue
   });
 };
@@ -39,15 +37,23 @@ describe("Dashboard.vue is a valid component", () => {
     expect(wrapper.findAll('[data-test="scan-card"]')).toHaveLength(3)
   })
 
-  it.only('correctly renders a Card', async () => {
+  it('correctly renders a Card', async () => {
     const wrapper = createComponent();
     const card = wrapper.findAll('[data-test="scan-card"]').at(0);
 
     expect(card.find('[data-test="card-title"]').text()).toEqual('Test Checkin1')
     expect(card.find('[data-test="card-action"]').text()).toEqual('SCAN')
+    expect(card.attributes()["href"]).toContain('/scan/test-checkin1')
+  });
 
-    // await card.trigger('click');
-    // expect(window.location.href).toContain("test-checkin1");
+  it('click on a Card, goto Scan Page', async () => {
+    const wrapper = createComponent();
+    const card = wrapper.findAll('[data-test="scan-card"]').at(0);
+
+    await card.trigger('click');
+
+    expect(wrapper.vm.$route.path).toContain("/scan/test-checkin1");
+    expect(window.location.href).toContain("/scan/test-checkin1");
   })
 });
 
